b36eafb6e3509709765dc8e33d2d554fc3dbfe9f
Muhdan Fyan Syah Sofian <muhdanfyan@gmail.com>
2021-09-06 10:41:21 +0800

    FE-Get
    FE Get API Consume

---
+++ frontend/src/components/ProductList.js
@@ -0,0 +1,46 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const ProductList = () => {
+    const [products, setProduct] = useState([]);
+
+    useEffect(() => {
+       getProducts();
+    },[]);
+
+    const getProducts = async () => {
+        const response = await axios.get ('http://localhost:5000/products');
+        setProduct(response.data);
+    }
+
+    return (
+        <div>
+            <table className="table is-striped is-fullwidth">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Title</th>
+                        <th>Price</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {products.map((product, index) => (
+                        <tr key={ product.id }>
+                            <td>{index +1 }</td>
+                            <td>{product.title}</td>
+                            <td>{product.price}</td>
+                            <td>
+                                <button className="button is-small is-info">Edit</button>
+                                <button className="button is-small is-danger">Delete</button>
+                            </td>
+                        </tr>
+                    ))}
+                    
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default ProductList
